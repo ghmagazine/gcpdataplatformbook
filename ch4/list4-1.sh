1145cd3ee6eced86b7875b1644637c2d460ba28b
Keiji Yoshida <kjmrknsn@gmail.com>
2020-11-02 20:39:14 +0900

    Create list4-1.sh

---
+++ ch4/list4-1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# リスト4-1. DataflowとDataprocのAPIの有効化
+# 以下のプロダクトのAPIを有効にします。
+# - Dataflow
+# - Dataproc
+gcloud services enable \
+  dataflow.googleapis.com \
+  dataproc.googleapis.com
