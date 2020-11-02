a490d8776788cbb860e5cef2db74b1234881569a
Keiji Yoshida <kjmrknsn@gmail.com>
2020-11-02 20:50:42 +0900

    Create list4-4.sh

---
+++ ch4/list4-4.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# リスト4-4. BigQueryのデータセットの作成
+# gcpbook_ch4という名前のBigQueryのデータセットを、
+# US マルチリージョンに作成します。
+bq --location=us mk \
+  -d \
+  $(gcloud config get-value project):gcpbook_ch4
