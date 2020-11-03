8fba9cdcdbcca78eb9734e693cd707742bf549e8
Keiji Yoshida <kjmrknsn@gmail.com>
2020-11-03 22:44:41 +0900

    Create list4-49.sh

---
+++ ch4/list4-49.sh
@@ -0,0 +1,6 @@
+# リスト4-49. Dataprocのワークフローテンプレートの作成
+# 前工程で作成したcount-users.yamlをインポートし、
+# Dataprocのワークフローテンプレートを作成します。
+gcloud dataproc workflow-templates import count-users \
+  --source=count-users.yaml \
+  --region=us-central1
