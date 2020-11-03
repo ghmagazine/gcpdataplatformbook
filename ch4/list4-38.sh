4911118fb103356c1741f93fbf7d7136f3ecf25a
Keiji Yoshida <kjmrknsn@gmail.com>
2020-11-03 22:41:04 +0900

    Create list4-38.sh

---
+++ ch4/list4-38.sh
@@ -0,0 +1,11 @@
+# リスト4-38. dauテーブルの再作成
+# 「日別の、課金ユーザと無課金ユーザそれぞれのユニークユーザ数」を
+# 保管するテーブルgcpbook_ch4.dauを再作成します。
+bq --location=us query \
+  --nouse_legacy_sql \
+  'create or replace table gcpbook_ch4.dau
+  (
+    dt date not null
+  , paid_users int64 not null
+  , free_to_play_users int64 not null
+  )'
