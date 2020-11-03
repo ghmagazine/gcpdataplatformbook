86cbcfffcf9d3cb9f7cb88c116b0d095b283a423
Keiji Yoshida <kjmrknsn@gmail.com>
2020-11-03 22:39:47 +0900

    Create list4-35.sh

---
+++ ch4/list4-35.sh
@@ -0,0 +1,8 @@
+# リスト4-35. Dataflowのジョブの実行
+python3 etl.py \
+  --region us-central1 \
+  --dt 20181001 \
+  --runner DataflowRunner \
+  --project $(gcloud config get-value project) \
+  --temp_location gs://$(gcloud config get-value project)-gcpbook-ch4/tmp/ \
+  --experiments shuffle_mode=service
